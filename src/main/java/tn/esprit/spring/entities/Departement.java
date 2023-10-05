5e42648c58e8fd4adc8b379f37b549750fbf432b
Zied Dridi <dridi.zied@esprit.tn>
2023-10-05 14:34:46 +0100

    First Commit

---
+++ src/main/java/tn/esprit/spring/entities/Departement.java
@@ -0,0 +1,47 @@
+package tn.esprit.spring.entities;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+
+@Entity
+@Getter
+@Setter
+public class Departement implements Serializable {
+
+	private static final long serialVersionUID = -357738161698377833L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	
+	@JsonIgnore
+	@ManyToMany(fetch=FetchType.EAGER, cascade=CascadeType.ALL)
+	private List<Employe> employes;
+	 
+	@ManyToOne
+	private Entreprise entreprise;
+
+	public Departement() {
+		super();
+	}
+
+	public Departement(String name) {
+		this.name = name;
+	}
+	 
+}
+ 
